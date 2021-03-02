f97cdefe3fed44891805d101118531a836514e8d
Jesse A. Houser <jesseahouser@gmail.com>
2021-03-01 20:32:40 -0500

    Cleaned up comments

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ newHeader.id = "victory"; // Sets id of `h1` element tp `victory'
 newHeader.textContent = "YOUR-NAME is the champion" // Sets text content of `h1`
 /* ALTERNATE METHOD OF ADDING TEXT
 const newHeaderText = document.createTextNode("YOUR-NAME is the champion"); // Creates text node
-//newHeader.appendChild(newHeaderText); // Places text node within `h1`
+newHeader.appendChild(newHeaderText); // Places text node within `h1`
 */
 document.body.appendChild(newHeader); // Paces `h1` within body
